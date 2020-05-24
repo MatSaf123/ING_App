2dc2edf26b073e6299c0e60b2565d69820483e97
TymJanek <jantanski1@gmail.com>
2020-05-24 15:10:00 +0200

    TODO: fix recycler view

--- app/src/main/java/com/msjtrs/ing_app/ui/CommentFragment.kt
+++ app/src/main/java/com/msjtrs/ing_app/ui/CommentFragment.kt
@@ -8,14 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.msjtrs.ing_app.adapters.CommentAdapter
 import com.msjtrs.ing_app.databinding.CommentFragmentBinding
 
 class CommentFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-
 
         val application = requireNotNull(activity).application
         val binding = CommentFragmentBinding.inflate(inflater)
@@ -27,6 +26,8 @@ class CommentFragment : Fragment() {
         val viewModelFactory = CommentViewModelFactory(postProperty, application)
 
         binding.viewModel = ViewModelProviders.of(this, viewModelFactory).get(CommentViewModel::class.java)
+
+        binding.commentsList.adapter = CommentAdapter()
 
         return binding.root
     }
