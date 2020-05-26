994f2bbf96e1d7dc1e2d885d172da37bf5d5c0ef
TymJanek <jantanski1@gmail.com>
2020-05-26 21:44:01 +0200

    added menu with info about authors and assets

--- app/src/main/java/com/msjtrs/ing_app/ui/OverviewFragment.kt
+++ app/src/main/java/com/msjtrs/ing_app/ui/OverviewFragment.kt
@@ -2,10 +2,12 @@ package com.msjtrs.ing_app.ui
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import com.msjtrs.ing_app.R
 import com.msjtrs.ing_app.adapters.CommentAdapter
 import com.msjtrs.ing_app.adapters.PostAdapter
 import com.msjtrs.ing_app.adapters.UserAdapter
@@ -16,6 +18,8 @@ class OverviewFragment : Fragment(){
     private val viewModel: OverviewViewModel by lazy {
         ViewModelProviders.of(this).get(OverviewViewModel::class.java)
     }
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?) : View? {
@@ -47,11 +51,35 @@ class OverviewFragment : Fragment(){
         })
 
 
-
-        //binding.usersList.adapter = UserAdapter()
-        //binding.commentsList.adapter = CommentAdapter()
-
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        setHasOptionsMenu(true)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item!!.itemId
+        if(id == R.id.authors){
+            Toast.makeText(activity,
+                "Authors:\n" +
+                        "Mateusz Safaryjski\n" +
+                        "Jan Tański\n" +
+                        "Robert Skubała\n\n" +
+                        "Assets taken from flaticon.com\n" +
+                        "Icon made by Freepik from www.flaticon.com\n" +
+                        "Icon made by Pixel perfect from www.flaticon.com\n\n" +
+                        "App version: 1.2"
+                        , Toast.LENGTH_LONG).show()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 
